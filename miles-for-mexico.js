cf31001c98de1b638fb0ac385cf379d2a7ddc1ba
ktmitton <kdrive113@gmail.com>
2024-02-02 12:43:48 -0500

    Put google stuff behind button click

--- miles-for-mexico.js
+++ miles-for-mexico.js
@@ -74,42 +74,44 @@ const initializeGoogleApiClient = (clientId, apiKey, scopes, discoveryDocs) => {
 
 await globalThis.loadGoogleScriptsTask;
 
-const CLIENT_ID = "339276363047-k52mkf39fe28d8j13kah8bmtpk0doehl.apps.googleusercontent.com";
-const API_KEY = "AIzaSyCaOkfOlCoiLPrgGUgRF4mYIjGjH-8plw8";
-const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://www.googleapis.com/discovery/v1/apis/oauth2/v1/rest"];
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+document.querySelector("#initiate")?.addEventListener("click", async() => {
+  const CLIENT_ID = "339276363047-k52mkf39fe28d8j13kah8bmtpk0doehl.apps.googleusercontent.com";
+  const API_KEY = "AIzaSyCaOkfOlCoiLPrgGUgRF4mYIjGjH-8plw8";
+  const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://www.googleapis.com/discovery/v1/apis/oauth2/v1/rest"];
+  const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
-await initializeGoogleApiClient(CLIENT_ID, API_KEY, SCOPES, DISCOVERY_DOCS);
+  await initializeGoogleApiClient(CLIENT_ID, API_KEY, SCOPES, DISCOVERY_DOCS);
 
-const emailAddress = (await gapi.client.oauth2.tokeninfo()).result.email;
+  const emailAddress = (await gapi.client.oauth2.tokeninfo()).result.email;
 
-document.querySelector("#logWorkout").addEventListener("click", async ({target}) => {
-  target.innerText = "Saving Workout..."
-  target.disabled = true;
+  document.querySelector("#logWorkout").addEventListener("click", async ({target}) => {
+    target.innerText = "Saving Workout..."
+    target.disabled = true;
 
-  const type = document.querySelector("#type").value;
-  const amount = document.querySelector("#amount").value;
-  const units = document.querySelector(".toggle")?.matches(".off") ? "kilometers" : "miles";
-  const notes = document.querySelector("#notes").value;
+    const type = document.querySelector("#type").value;
+    const amount = document.querySelector("#amount").value;
+    const units = document.querySelector(".toggle")?.matches(".off") ? "kilometers" : "miles";
+    const notes = document.querySelector("#notes").value;
 
-  const request = {
-    spreadsheetId: "1TgwZSql7rRbP-E7Fs6_jZ2DifwZ82kpnZoV5MfkW_UI",
-    range: "2024!A:F",
-    valueInputOption: "USER_ENTERED",
-    insertDataOption: 'INSERT_ROWS',
-    resource: {
-        "majorDimension": "ROWS",
-        "values": [
-            [new Date(), emailAddress, type, amount, units, notes],
-        ]
-    },
-  };
-  const result = await gapi.client.sheets.spreadsheets.values.append(request);
+    const request = {
+      spreadsheetId: "1TgwZSql7rRbP-E7Fs6_jZ2DifwZ82kpnZoV5MfkW_UI",
+      range: "2024!A:F",
+      valueInputOption: "USER_ENTERED",
+      insertDataOption: 'INSERT_ROWS',
+      resource: {
+          "majorDimension": "ROWS",
+          "values": [
+              [new Date(), emailAddress, type, amount, units, notes],
+          ]
+      },
+    };
+    const result = await gapi.client.sheets.spreadsheets.values.append(request);
 
-  alert("Workout Saved!");
+    alert("Workout Saved!");
 
-  target.innerText = "Save Workout"
-  target.disabled = false;
+    target.innerText = "Save Workout"
+    target.disabled = false;
+  });
 });
 
 export {};
